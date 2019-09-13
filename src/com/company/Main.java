a4068784ba4e24344d46029fde8d5ae84a9c1bfe
rachsb5 <rachaelhornsby@billericak12.com>
2019-09-13 11:27:32 -0400

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Goodbye");
+        //double x = Math.random();   // random number [0.0,1.0)
+        //double x = Math.random()*10;   // random double [0.0,10.0)
+        //int x = (int)(Math.random()*10);  //random int [0,10)
+        //int x = (int)(Math.random()*20);
+        int x = (int)(Math.random()*10 + 10);  // random int [10,20)
+        System.out.println(x);
     }
 }
