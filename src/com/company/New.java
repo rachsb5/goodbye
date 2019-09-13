557adf7b1acdf02b4b7d2ab81f3e0a9bf62e2366
rachsb5 <rachaelhornsby@billericak12.com>
2019-09-13 12:29:15 -0400

    initial commit

---
+++ src/com/company/New.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class MoreMethods {
+    public static void main(String[] args)
+    {
+        int a = method();
+        System.out.println(z);
+    }
+    public static int method () {
+        int x = 4;
+        int y = 5;
+        int z=x*y;
+        return z;
+    }
+    public static double method2();
+    {
+        double x = 3.5;
+        
+    }
+
+}
